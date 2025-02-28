1634aaf8d60c5dd1dcbc179e3854ad23274142dc
BT_2S14_19 <arshpurohit@gmail.com>
2025-02-28 10:48:43 -0500

    Some commit idek dawg

--- src/SimpleMovie.java
+++ src/SimpleMovie.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class SimpleMovie {
     private String title;
@@ -13,10 +16,26 @@ public class SimpleMovie {
         for (int i = 0; i < tempActors.length; i++) {
             actors.add(tempActors[i]);
         }
+    }
 
+    class Node {
+        String data;
+        Node left, right;
+        Node(String d) {
+            data = d;
+            left = null;
+            right = null;
+        }
+    }
+
+    public void findShortestPath() {
+        List<Node> Tree = new LinkedList<Node>();
+        for (int i = 0; i < actors.size(); i++) {
+            Tree.add(actors.get(i));
+        }
     }
 
     public String toString() {
         return "Title: " + title + "\n" + "Actors: " + actors + "\n";
+        }
     }
-}
